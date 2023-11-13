c94ba5a076ecdb97e59a99891ce201ca2be23baa
chukkanagapraneeth <cnagapraneeth@gmail.com>
2023-11-13 19:27:39 +0530

    Add project files.

---
+++ ModelBinding/Contollers/ModelController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Contollers
+{
+    public class ModelController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        //Url: /bookstore?bookid=10&isloggedin=true
+        public IActionResult Index(int? bookid, bool? isloggedin, Book book)
+        {
+            //Book id should be applied
+            if (bookid.HasValue == false)
+            {
+                //return new BadRequestResult();
+                return BadRequest("Book id is not supplied or empty");
+            }
+
+            //Book id can't be less than or equal to 0
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to 0");
+            }
+
+            //Book id should be between 1 to 1000
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            //isloggedin should be true
+            if (isloggedin == false)
+            {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookid}, Book: {book}", "text/plain");
+        }
+
+        [Route("user")]
+        public IActionResult User(User usr)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(String.Join("\n",ModelState.Values.SelectMany(val => val.Errors).Select(msg => msg.ErrorMessage).ToList()));
+            }
+            else
+            {
+                return Json(usr);
+            }
+        }
+
+    }
+}
---
+++ ModelBinding/Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBinding.Models
+{
+    public class Book
+    {
+        public string? Author { get; set; }
+        [FromQuery]
+        public int? Bookid { get; set; }
+
+        public override string ToString()
+        {
+            return $"Bookid : {Bookid}, Author: {Author}";
+        }
+    }
+
+    public class User
+    {
+        [Required(ErrorMessage = "{0} is Empty")]
+        [Display(Name = "Peru")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "{0} should be {2} and {1} lenghth long")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is Empty")]
+        [EmailAddress(ErrorMessage = "Give a proper email g")]
+        public string? Email { get; set; }
+
+        [Range(20, maximum: 65, ErrorMessage = "{0} should be between {1} and {2}")]
+        public int Age { get; set; }
+
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "only alpha space dot chars are allowed")]
+        public string? School { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Password must be between {1} to {2} characters")]
+        [Required]
+        public string? password { get; set; }
+
+        [Compare("password", ErrorMessage = "{0} Doesn't match with password")]
+        [Required]
+        public string? confirmpassword { get; set; }
+
+    }
+}
