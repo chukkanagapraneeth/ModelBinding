9d969e4fb3ae975859987718c6d94f34bb437029
chukkanagapraneeth <cnagapraneeth@gmail.com>
2023-11-19 13:40:08 +0530

    Implemented IValidatableObject and Reflection multiple properties

---
+++ ModelBinding/CustomValidators/DateRangeValidatorAttribute.cs
@@ -0,0 +1,43 @@
+﻿using ModelBinding.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ModelBinding.CustomValidators
+{
+    public class DateRangeValidatorAttribute : ValidationAttribute
+    {
+        public string? PropertyName { get; set; }
+        
+        public DateRangeValidatorAttribute(string OtherPropertyName)
+        {
+            PropertyName = OtherPropertyName;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {              
+            if (value != null)
+            {
+                DateTime to_date = Convert.ToDateTime(value);
+
+                if (PropertyName != null)
+                {
+                    var model = (User)validationContext.ObjectInstance;
+                    PropertyInfo? otherproperty = validationContext.ObjectType.GetProperty(PropertyName);
+
+                    DateTime from_date = Convert.ToDateTime(otherproperty.GetValue(validationContext.ObjectInstance));
+
+                    if(from_date < to_date)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+                {
+                    return new ValidationResult("Please provide From Date");
+                }
+            }
+            else
+            {
+                return new ValidationResult("Please provide To Date");
+            }
+        }
+    }
+}
--- ModelBinding/Models/Book.cs
+++ ModelBinding/Models/Book.cs
@@ -16,7 +16,7 @@ namespace ModelBinding.Models
         }
     }
 
-    public class User
+    public class User : IValidatableObject
     {
         [Required(ErrorMessage = "{0} is Empty")]
         [Display(Name = "Peru")]
@@ -28,7 +28,7 @@ namespace ModelBinding.Models
         public string? Email { get; set; }
 
         [Range(20, maximum: 65, ErrorMessage = "{0} should be between {1} and {2}")]
-        public int Age { get; set; }
+        public int? Age { get; set; }
 
         [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "only alpha space dot chars are allowed")]
         public string? School { get; set; }
@@ -47,5 +47,19 @@ namespace ModelBinding.Models
         [DobValidation(DOB: "2000-12-12", ErrorMessage = "Year should be less than {0} for {1} parameter")]
         public DateTime? DateOfBirth { get; set; }
 
+        //Multiple Properties using Reflection
+        public string? FromDate { get; set; }
+
+        [DateRangeValidator("FromDate", ErrorMessage = "From Date should be less than To Date.")]
+        public string? ToDate { get; set; }
+
+        //IMPLEMENTING IVALIDATABLEOBJECT
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(DateOfBirth.HasValue == false && Age.HasValue == false)
+            {
+                yield return new ValidationResult("Either date of birth or Age must be supplied", new[] { nameof(DateOfBirth)});
+            } 
+        }
     }
 }
